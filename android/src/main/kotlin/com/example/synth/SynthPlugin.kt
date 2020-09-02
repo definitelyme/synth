592b90f8c3c0f74385e43c8acec02ed8f665e15e
Brendan Ejike <ejike.br@gmail.com>
2020-09-02 03:06:49 +0100

    Design plugin API

--- android/src/main/kotlin/com/example/synth/SynthPlugin.kt
+++ android/src/main/kotlin/com/example/synth/SynthPlugin.kt
@@ -8,29 +8,55 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import com.example.synth.Synth
 
 /** SynthPlugin */
-class SynthPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class SynthPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private final var channelName = "synth"
+    private lateinit var channel: MethodChannel
+    private lateinit var synth: Synth
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "synth")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    fun setup(plugin: SynthPlugin, flutterPluginBinding: FlutterPlugin.FlutterPluginBinding): Unit {
+        plugin.channel = MethodChannel(flutterPluginBinding.binaryMessenger, channelName)
+        plugin.channel.setMethodCallHandler(plugin)
+        plugin.synth = Synth()
+        plugin.synth.start()
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        setup(this, flutterPluginBinding);
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getPlatformVersion" -> result.success("Android version ${android.os.Build.VERSION.RELEASE}")
+            "keyDown" -> {
+                try {
+                    val arguments = call.arguments as ArrayList<*>
+                    val numKeysDown = synth.keyDown(arguments.get(0) as Int)
+                    result.success(numKeysDown)
+                } catch (ex: Exception) {
+                    result.error("1", ex.message, ex.getStackTrace())
+                }
+            }
+            "keyUp" -> {
+                try {
+                    val arguments = call.arguments as ArrayList<*>
+                    val numKeysUp = synth.keyUp(arguments.get(0) as Int)
+                    result.success(numKeysUp)
+                } catch (ex: Exception) {
+                    result.error("1", ex.message, ex.getStackTrace())
+                }
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
--- lib/synth.dart
+++ lib/synth.dart
@@ -5,8 +5,19 @@ import 'package:flutter/services.dart';
 class Synth {
   static const MethodChannel _channel = const MethodChannel('synth');
 
+  /// Returns the running Platform Version
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<int> onKeyDown(int key) async {
+    final int numNotesOn = await _channel.invokeMethod('keyDown', [key]);
+    return numNotesOn;
+  }
+
+  static Future<int> onKeyUp(int key) async {
+    final int numNotesOn = await _channel.invokeMethod('keyUp', [key]);
+    return numNotesOn;
   }
 }
